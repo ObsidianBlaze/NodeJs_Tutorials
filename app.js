839c1681dc3c4e3f8c41354659f9f9ec0e61f121
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2022-01-14 01:01:18 +0100

    Came back to Node after leaving it for Laravel in 2020 :)

--- app.js
+++
@@ -1,6 +0,0 @@
-const tutorial = require('./tutorial');
-
-console.log(tutorial.sum(1, 2));
-console.log(tutorial.PI);
-console.log(new tutorial.Users());
-
---
+++ demo.js
@@ -0,0 +1,6 @@
+const tutorial = require('./tutorial');
+
+console.log(tutorial.sum(1, 2));
+console.log(tutorial.PI);
+console.log(new tutorial.Users());
+
